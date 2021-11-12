e5cbaccd7ed434086ddb1f315eae75453f17b8b6
Eric Jiang <ejiang@amazon.com>
2021-11-11 23:40:56 -0700

    Create custom runtime.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,10 @@
+import io.circe.generic.auto._
+import me.ericjiang.aws.lambda.scalaruntime.LambdaRuntime
+
 object Main extends App {
   println("Hello, World!")
+  case class Request(name: String)
+  case class Response(message: String)
+  def handler(request: Request): Response = Response(s"Hello, ${request.name}")
+  new LambdaRuntime().run(handler _)
 }
